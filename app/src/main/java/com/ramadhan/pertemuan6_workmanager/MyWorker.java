4cfd11fae64f111b732e3cd363ffdf5ea0b8c6f1
ramadhansur <ramadhansuryanto44@gmail.com>
2021-11-22 16:30:32 +0700

    first commit

---
+++ app/src/main/java/com/ramadhan/pertemuan6_workmanager/MyWorker.java
@@ -0,0 +1,38 @@
+package com.ramadhan.pertemuan6_workmanager;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class MyWorker extends Worker {
+    public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @NonNull
+    @Override
+    public Result doWork() {
+        displayNotification("Ini adalah Task Kamu","Task kamu  sudah selesai");
+        return Result.success();
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void displayNotification(String task, String desc){
+        NotificationManager manager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel channel = new NotificationChannel("terserah", "test123", NotificationManager.IMPORTANCE_HIGH);
+        manager.createNotificationChannel(channel);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "terserah")
+                .setContentTitle(task)
+                .setContentText(desc)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE);
+        manager.notify(1, builder.build());
+    }
+}
